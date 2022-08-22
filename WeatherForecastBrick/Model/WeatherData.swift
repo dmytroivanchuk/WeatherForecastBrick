d021b99d96bf95c354ad2ec0894576b93eaef6a5
Dmytro Ivanchuk <dmytroivanchuk1998@gmail.com>
2022-08-23 01:07:47 +0300

    create WeatherData, WeatherModel, WeatherManager structs

---
+++ WeatherForecastBrick/Model/WeatherData.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherData.swift
+//  WeatherForecastBrick
+//
+//  Created by Dmytro Ivanchuk on 23.08.2022.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
